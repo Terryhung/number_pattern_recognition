4a785c38a508cb2e836ef679528aad29dbc1dc40
TerryHung <B00201015@gmail.com>
2014-12-16 15:31:32 +0800

    add new alg

--- learner_linearsvm.py
+++ learner_linearsvm.py
@@ -1,4 +1,4 @@
-from sklearn import ensemble, datasets, grid_search, svm
+from sklearn import datasets, grid_search, svm
 import numpy as np
 
 if __name__ == "__main__":
@@ -6,14 +6,13 @@ if __name__ == "__main__":
     X_train, y_train = datasets.load_svmlight_file('ml14fall_train.dat', n_features="12810")
     x_test, y_test = datasets.load_svmlight_file('ml14fall_test1_no_answer.dat', n_features="12810")
     y_train = y_train.astype(int)
-    for i in range(22, 32):
-        y_train[y_train == i] = i-10
 
     # algorithm: random forest
     svc = svm.LinearSVC()
-    parameters = {'C': [0.1, 1, 10, 100, 1000], 'multi_class': ['ovr', 'crammer_singer']}
-    clf = grid_search.GridSearchCV(svc, parameters, n_jobs=5, verbose=100)
+    parameters = {'C': [0.01, 0.1, 1, 10, 100], 'multi_class': ['ovr', 'crammer_singer']}
+    clf = grid_search.GridSearchCV(svc, parameters, n_jobs=8, verbose=10)
     clf.fit(X_train.toarray(), y_train)
+
     print clf.best_params_
     y_predict = clf.best_estimator_.predict(x_test.toarray())
-    np.savetxt("svm_linear.txt", y_predict, fmt="%s", newline='\n')
+    np.savetxt("svm_linear_v2.txt", y_predict, fmt="%s", newline='\n')
