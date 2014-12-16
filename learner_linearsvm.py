7ec9770b4467dae070f94c67b2b383c8e3799fa6
TerryHung <B00201015@gmail.com>
2014-12-16 10:26:23 +0800

    Add new gird search in linear svm

--- learner_linearsvm.py
+++ learner_linearsvm.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     # algorithm: random forest
     svc = svm.LinearSVC()
     parameters = {'C': [0.1, 1, 10, 100, 1000], 'multi_class': ['ovr', 'crammer_singer']}
-    clf = grid_search.GridSearchCV(svc, parameters, n_jobs=5)
+    clf = grid_search.GridSearchCV(svc, parameters, n_jobs=5, verbose=100)
     clf.fit(X_train.toarray(), y_train)
     print clf.best_params_
     y_predict = clf.best_estimator_.predict(x_test.toarray())
