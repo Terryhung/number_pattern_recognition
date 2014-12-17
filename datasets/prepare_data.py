f9131a44c56eec6f1656c9d9a71e7f72b5f686f8
TerryHung <B00201015@gmail.com>
2014-12-17 22:55:47 +0800

    Add new tool: prepare_data

---
+++ datasets/prepare_data.py
@@ -0,0 +1,22 @@
+from sklearn import datasets, cross_validation
+import numpy as np
+import scipy as sp
+
+X_train, y_train = datasets.load_svmlight_file('ml14fall_train.dat', n_features="12810", dtype="float64")
+X_test, y_test = datasets.load_svmlight_file('ml14fall_test1_no_answer.dat', n_features="12810", dtype="float64")
+
+X_train, X_cv, y_train, y_cv = cross_validation.train_test_split(X_train,
+                                                                 y_train,
+                                                                 test_size=0.2,
+                                                                 random_state=12)
+
+X_train = X_train.toarray()
+X_cv = X_cv.toarray()
+X_test = X_test.toarray()
+
+np.save('X_train', X_train)
+np.save('y_train', y_train)
+np.save('X_cv', X_cv)
+np.save('y_cv', y_cv)
+np.save('X_test', X_test)
+np.save('y_test', y_test)
