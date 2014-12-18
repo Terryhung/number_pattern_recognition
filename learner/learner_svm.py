7f0bf82c4d0477dccf2c420c902076da320dd0b0
TerryHung <B00201015@gmail.com>
2014-12-18 11:24:08 +0800

    Add new tool: add model

--- learner/learner_svm.py
+++ learner/learner_svm.py
@@ -1,19 +1,30 @@
 from sklearn import ensemble, datasets, grid_search, svm
 import numpy as np
 from my_tool import read_data
+from my_tool import add_model
+
 if __name__ == "__main__":
     # data pre
     X_train, y_train = datasets.load_svmlight_file(read_data('hog_ml14fall_train.dat'), n_features="12810")
     x_test, y_test = datasets.load_svmlight_file(read_data('hog_ml14fall_test1_no_answer.dat'), n_features="12810")
     y_train = y_train.astype(int)
 
-    # algorithm: random forest
+    # algorithm: svm
     svc = svm.SVC()
     parameters = [
-        {'C': [1, 10, 100, 1000], 'gamma': [0.001, 0.0001, 0.1, 0.01], 'kernel': ['rbf']},
+        {'C': [100],
+         'gamma': [0.1],
+         'kernel': ['rbf']},
     ]
     clf = grid_search.GridSearchCV(svc, parameters, n_jobs=10)
     clf.fit(X_train.toarray(), y_train)
+
     print clf.best_params_
     y_predict = clf.best_estimator_.predict(x_test.toarray())
-    np.savetxt("svm_kernel_hog.txt", y_predict, fmt="%s", newline='\n')
+    np.savetxt("../answer/svm_kernel_hog.txt",
+               y_predict, fmt="%s", newline='\n')
+
+    # Save model
+    print 'Do you want to save this model? <N | filename>'
+    answer = raw_input()
+    add_model(answer, clf)
--- learner/my_tool.py
+++ learner/my_tool.py
@@ -1,4 +1,6 @@
 import os
+import errno
+import dill
 
 
 def read_data(filename):
@@ -13,3 +15,18 @@ def save_data(filename):
     rel_path = "../answer/" + filename
     abs_file_path = os.path.join(path, rel_path)
     return abs_file_path
+
+
+def add_model(answer, clf):
+    if answer != 'n' and answer != 'N':
+        try:
+            os.makedirs('../models')
+        except OSError, exc:
+            if exc.errno == errno.EEXIST and os.path.isdir('../models'):
+                pass
+            else:
+                raise
+
+        with open(os.path.join('../models', answer), 'w') as f:
+            f.write(dill.dumps(clf.best_estimator_))
+            print 'Model written'
