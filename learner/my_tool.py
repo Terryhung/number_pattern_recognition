dba22836db42c4fba376a47856b43ff58510a733
TerryHung <B00201015@gmail.com>
2014-12-18 10:57:53 +0800

    Add new tool in my_tool: save_data

--- learner/my_tool.py
+++ learner/my_tool.py
@@ -1,8 +1,15 @@
 import os
-import sys
+
 
 def read_data(filename):
     path = os.path.dirname(__file__)
     rel_path = "../datasets/" + filename
+    abs_file_path = os.path.join(path, rel_path)
+    return abs_file_path
+
+
+def save_data(filename):
+    path = os.path.dirname(__file__)
+    rel_path = "../answer/" + filename
     abs_file_path = os.path.join(path, rel_path)
     return abs_file_path
