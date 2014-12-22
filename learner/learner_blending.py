76a9249f197d492c39435dfed4282fbbd4fcfe97
terryhung <terryhung28@cml21.csie.ntu.edu.tw>
2014-12-22 23:13:09 +0800

    add new thing

--- learner/learner_blending.py
+++ learner/learner_blending.py
@@ -5,39 +5,23 @@ from my_tool import add_model
 from my_tool import load_model
 
 if __name__ == "__main__":
-    # data pre
-    x_test, y_test = datasets.load_svmlight_file(
-        read_data('hog_ml14fall_test1_no_answer.dat'), n_features="12810")
-    print ("Finish loading data")
-
-    # algorithm: svm and linear svm and regression
-    svc_linear_bagging = load_model('linear_svm_bagging')
-    svc_linear = load_model('linear_svm')
-    svc_kernel = load_model('svm_kernel')
-    svc_kernel_125 = load_model('svm_kernel_125')
-    linear_regression = load_model('regression')
-    print ("Finish loading model")
-
     # start blending
     print ("start predict")
-    y_kernel = svc_kernel.predict(x_test)
-    np.savetxt("./blending_data/kernel.txt",
-               y_kernel, fmt="%s", newline='\n')
-    y_kernel_125 = svc_kernel_125.predict(x_test)
-    np.savetxt("./blending_data/kernel_125.txt",
-               y_kernel_125, fmt="%s", newline='\n')
-    y_linear_bagging = svc_linear_bagging.predict(x_test)
-    np.savetxt("./blending_data/linear_bagging.txt",
-               y_linear_bagging, fmt="%s", newline='\n')
-    y_linear = svc_linear.predict(x_test)
-    np.savetxt("./blending_data/linear.txt",
-               y_linear, fmt="%s", newline='\n')
-    y_regression = linear_regression.predict(x_test)
-    np.savetxt("./blending_data/regression.txt",
-               y_regression, fmt="%s", newline='\n')
+    y_kernel = np.loadtxt('./blending_data/svm_test.txt')
+    y_kernel_125 = np.loadtxt('./blending_data/kernel_125.txt')
+    y_linear_bagging = np.loadtxt('./blending_data/linear_bagging.txt')
+    y_regression = np.loadtxt('./blending_data/regression.txt')
     print ("Finish predict")
+    
+    y = np.vstack((y_kernel, y_kernel_125, y_linear_bagging, y_regression)).T
+    y_prediction = []
+    for i in range(y.shape[0]):
+        unique,pos = np.unique(y[i],return_inverse=True)
+        counts = np.bincount(pos)
+        maxpos = counts.argmax()
+        y_prediction.append(int(unique[maxpos]))
+        print ("finish"+str(i))
+    y_prediction = np.asarray(y_prediction)
+    np.savetxt("../answer/blending.txt",
+                y_prediction, fmt="%s", newline='\n')
 
-    # err output
-    # print ("start test")
-    # np.savetxt("../answer/blending_hog.txt",
-    #             y, fmt="%s", newline='\n')
